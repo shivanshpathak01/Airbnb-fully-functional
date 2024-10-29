0159f55a1e8aac9e299047841a2f365c563851f0
Shivansh Pathak <shivanshpathak0001@gmail.com>
2024-10-29 10:46:53 +0530

    Till index route

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Listing = require("../Airbnb-fully-functional/models/listing.js");
+const path = require("path");
 
 // Connecting DB
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
@@ -18,22 +19,31 @@ async function main(){
     await mongoose.connect(MONGO_URL);
 }
 
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"))
+
 app.get("/",(req,res)=>{        // making APIs
     res.send("Hi, I am root"); 
 })
 
-app.get("/testListing",async (req,res)=>{
-    let sampleListing = new Listing({
-    title:"My Home",
-    description: "near Railway Station",
-    price: 1200,
-    location: "Ayodhya",
-    country: "India",
-    });
-    await sampleListing.save();
-    console.log("Sample was saved successfully");
-    res.send("Successful");
-});
+// Index Route 
+app.get("/listings",async (req,res)=>{
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs",{allListings})
+})
+
+// app.get("/testListing",async (req,res)=>{
+//     let sampleListing = new Listing({
+//     title:"My Home",
+//     description: "near Railway Station",
+//     price: 1200,
+//     location: "Ayodhya",
+//     country: "India",
+//     });
+//     await sampleListing.save();
+//     console.log("Sample was saved successfully");
+//     res.send("Successful");
+// });
 
 app.listen(8080,()=>{
     console.log("server is listening to port 8080");  
--- models/listing.js
+++ models/listing.js
@@ -4,19 +4,22 @@ const Schema = mongoose.Schema;
 // Making the schema on which we have to work 
 const listingSchema = new Schema({
     title: {
-        type : String,
-        required : true
+        type: String,
+        required: true
     },
     description: String,
     image: {
-        type : String,
-        default:"https://www.pexels.com/photo/concrete-road-between-trees-1563356/",
-        set: (v)=> v ===""?"https://www.pexels.com/photo/concrete-road-between-trees-1563356/":v,      // To set default value for images we use v 
-    },    
+        // Update the image field to be an object with filename and url
+        filename: { type: String, default: "listingimage" },
+        url: {
+            type: String,
+            default: "https://www.pexels.com/photo/concrete-road-between-trees-1563356/"
+        },
+    },
     price: Number,
     location: String,
     country: String,
 });
 
-const Listing = mongoose.model("Listing",listingSchema);
+const Listing = mongoose.model("Listing", listingSchema);
 module.exports = Listing;
